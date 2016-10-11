8fe8c60c683362cd76cc5b6963c32900c98802f0
RaenonX JELLYCAT <maplestory0710@gmail.com>
2016-10-12 03:09:52 +0800

    TGA to PNG batch converter

---
+++ TargaImageBatchConverter/Main.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace TargaImageBatchConverter
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PendingFile = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SelectFiles = new System.Windows.Forms.Button();
+            this.Convert = new System.Windows.Forms.Button();
+            this.Progress = new System.Windows.Forms.ProgressBar();
+            this.ClearList = new System.Windows.Forms.Button();
+            this.SelectPath = new System.Windows.Forms.Button();
+            this.SPath = new System.Windows.Forms.Label();
+            this.ProgressText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // PendingFile
+            // 
+            this.PendingFile.FormattingEnabled = true;
+            this.PendingFile.ItemHeight = 16;
+            this.PendingFile.Location = new System.Drawing.Point(12, 77);
+            this.PendingFile.Name = "PendingFile";
+            this.PendingFile.Size = new System.Drawing.Size(664, 244);
+            this.PendingFile.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F);
+            this.label1.Location = new System.Drawing.Point(12, 55);
+            this.label1.Margin = new System.Windows.Forms.Padding(3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Pending File";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // SelectFiles
+            // 
+            this.SelectFiles.Location = new System.Drawing.Point(12, 12);
+            this.SelectFiles.Name = "SelectFiles";
+            this.SelectFiles.Size = new System.Drawing.Size(99, 37);
+            this.SelectFiles.TabIndex = 2;
+            this.SelectFiles.Text = "Add Files";
+            this.SelectFiles.UseVisualStyleBackColor = true;
+            // 
+            // Convert
+            // 
+            this.Convert.Font = new System.Drawing.Font("Microsoft JhengHei", 9F);
+            this.Convert.Location = new System.Drawing.Point(577, 327);
+            this.Convert.Name = "Convert";
+            this.Convert.Size = new System.Drawing.Size(99, 36);
+            this.Convert.TabIndex = 5;
+            this.Convert.Text = "CONVERT";
+            this.Convert.UseVisualStyleBackColor = true;
+            // 
+            // Progress
+            // 
+            this.Progress.Location = new System.Drawing.Point(12, 327);
+            this.Progress.Name = "Progress";
+            this.Progress.Size = new System.Drawing.Size(559, 14);
+            this.Progress.TabIndex = 6;
+            // 
+            // ClearList
+            // 
+            this.ClearList.Location = new System.Drawing.Point(629, 49);
+            this.ClearList.Name = "ClearList";
+            this.ClearList.Size = new System.Drawing.Size(47, 22);
+            this.ClearList.TabIndex = 7;
+            this.ClearList.Text = "Clear";
+            this.ClearList.UseVisualStyleBackColor = true;
+            // 
+            // SelectPath
+            // 
+            this.SelectPath.Location = new System.Drawing.Point(117, 12);
+            this.SelectPath.Name = "SelectPath";
+            this.SelectPath.Size = new System.Drawing.Size(99, 37);
+            this.SelectPath.TabIndex = 8;
+            this.SelectPath.Text = "Select Path";
+            this.SelectPath.UseVisualStyleBackColor = true;
+            // 
+            // SPath
+            // 
+            this.SPath.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.SPath.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F);
+            this.SPath.Location = new System.Drawing.Point(222, 12);
+            this.SPath.Margin = new System.Windows.Forms.Padding(3);
+            this.SPath.Name = "SPath";
+            this.SPath.Size = new System.Drawing.Size(454, 37);
+            this.SPath.TabIndex = 9;
+            this.SPath.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // ProgressText
+            // 
+            this.ProgressText.Font = new System.Drawing.Font("Microsoft JhengHei", 8.25F);
+            this.ProgressText.Location = new System.Drawing.Point(336, 347);
+            this.ProgressText.Margin = new System.Windows.Forms.Padding(3);
+            this.ProgressText.Name = "ProgressText";
+            this.ProgressText.Size = new System.Drawing.Size(235, 16);
+            this.ProgressText.TabIndex = 10;
+            this.ProgressText.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(688, 377);
+            this.Controls.Add(this.ProgressText);
+            this.Controls.Add(this.SPath);
+            this.Controls.Add(this.SelectPath);
+            this.Controls.Add(this.ClearList);
+            this.Controls.Add(this.Progress);
+            this.Controls.Add(this.Convert);
+            this.Controls.Add(this.SelectFiles);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PendingFile);
+            this.Font = new System.Drawing.Font("Microsoft JhengHei", 9F);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox PendingFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button SelectFiles;
+        private System.Windows.Forms.Button Convert;
+        private System.Windows.Forms.ProgressBar Progress;
+        private System.Windows.Forms.Button ClearList;
+        private System.Windows.Forms.Button SelectPath;
+        private System.Windows.Forms.Label SPath;
+        private System.Windows.Forms.Label ProgressText;
+    }
+}
+
---
+++ TargaImageBatchConverter/Main.cs
@@ -0,0 +1,150 @@
+﻿using Paloma;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace TargaImageBatchConverter
+{
+    public partial class Main : Form
+    {
+        BackgroundWorker BGW = new BackgroundWorker();
+
+        public Main()
+        {
+            InitializeComponent();
+            SPath.Text = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/Converted";
+
+            SelectFiles.Click += SelectFiles_Click;
+            Convert.Click += Convert_Click;
+            ClearList.Click += ClearList_Click;
+            SelectPath.Click += SelectPath_Click;
+
+            BGW.WorkerReportsProgress = true;
+            BGW.WorkerSupportsCancellation = true;
+            BGW.DoWork += BGW_DoWork;
+            BGW.RunWorkerCompleted += BGW_RunWorkerCompleted;
+            BGW.ProgressChanged += BGW_ProgressChanged;
+        }
+
+        private void Merge_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void BGW_DoWork(object sender, DoWorkEventArgs e)
+        {
+            bool ReplaceFlag = false, NameDiffFlag = false;
+            int FileCount = PendingFile.Items.Count, Counter = 0;
+
+            if (!Directory.Exists(SPath.Text))
+                Directory.CreateDirectory(SPath.Text);
+
+            foreach (object item in PendingFile.Items)
+            {
+                if (BGW.CancellationPending)
+                {
+                    BGW.ReportProgress(0, "Abort");
+                    break;
+                }
+
+                string FCompletePath = item.ToString();
+                string FDirectory = Path.GetDirectoryName(Path.GetDirectoryName(Path.GetDirectoryName(FCompletePath))).Split(new char[]{'\\', '/'}).Last();
+                string FName = Path.GetFileNameWithoutExtension(FCompletePath);
+                string SCompletePath = SPath.Text + "/" + FDirectory + " - " + FName + ".png";
+
+                if (!NameDiffFlag && !ReplaceFlag && File.Exists(SCompletePath))
+                {
+                    DialogResult Res = MessageBox.Show("File name conflicted. Settings you choose later will apply on all files.\nClick \"Retry\" to Name as different.\nClick \"Ignore\" to replace.\nClick \"Abort\" to cancel whole work.", "File Confliction", MessageBoxButtons.AbortRetryIgnore);
+                    switch (Res)
+                    {
+                        case DialogResult.Abort:
+                            BGW.CancelAsync();
+                            continue;
+                        case DialogResult.Ignore:
+                            ReplaceFlag = true;
+                            break;
+                        case DialogResult.Retry:
+                            NameDiffFlag = true;
+                            break;
+                    }
+                }
+
+                if (NameDiffFlag)
+                {
+                    SCompletePath = SPath.Text + "/" + FDirectory + " - " + FName + Guid.NewGuid().ToString().Substring(0, 4) + ".png";
+                }
+
+                TargaImage TGA = new TargaImage(FCompletePath);
+                TGA.Image.Save(SCompletePath, ImageFormat.Png);
+                Counter++;
+                BGW.ReportProgress(Counter / FileCount * 100, Counter + "/" + FileCount);
+            }
+        }
+
+        private void BGW_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (e.UserState.ToString() == "Abort")
+            {
+                Convert.Enabled = true;
+                MessageBox.Show("TGA to PNG converting aborted.");
+            } else
+            {
+                ProgressText.Text = e.UserState.ToString();
+                Progress.Value = e.ProgressPercentage;
+            }
+        }
+
+        private void BGW_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            Convert.Enabled = true;
+            MessageBox.Show("TGA to PNG converting completed.");
+        }
+
+        private void Convert_Click(object sender, EventArgs e)
+        {
+            Convert.Enabled = false;
+            Progress.Value = 0;
+            BGW.RunWorkerAsync();
+        }
+
+        private void SelectFiles_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OPF = new OpenFileDialog();
+            OPF.Filter = "Targa File|*.tga";
+            OPF.Multiselect = true;
+            OPF.Title = "Select files to convert images from TGA to PNG.";
+            if (OPF.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string FName in OPF.FileNames)
+                {
+                    PendingFile.Items.Add(FName);
+                }
+            }
+        }
+
+        private void SelectPath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FLD = new FolderBrowserDialog();
+            FLD.Description = "Choose the folder you want to save PNGs.";
+            FLD.RootFolder = Environment.SpecialFolder.Desktop;
+            FLD.ShowNewFolderButton = true;
+            if (FLD.ShowDialog() == DialogResult.OK)
+            {
+                SPath.Text = FLD.SelectedPath;
+            }
+        }
+
+        private void ClearList_Click(object sender, EventArgs e)
+        {
+            PendingFile.Items.Clear();
+        }
+    }
+}
